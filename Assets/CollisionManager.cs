95e103dc52a6287ab2774cb1f2b70089b961e39f
Ignacio Antonio Peralta Muñoz <ignacio.peralta9085@alumnos.udg.mx>
2026-04-05 14:01:20 -0600

    Actividad 2.6: Estados de animaciónm con Animator e interacción entre escenas.

--- Assets/CollisionManager.cs
+++ Assets/CollisionManager.cs
@@ -2,8 +2,31 @@ using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
+    public Animator bubble1Animator;
+    public Animator bubble2Animator;
+    public Animator bubble3Animator;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Colisión detectada con: " + other.gameObject.name);
+        if (other.gameObject.name == "Fuego")
+        {
+            Debug.Log("Unity está sobre el fuego");
+
+            bubble1Animator.SetBool("isBubbling", true);
+            bubble2Animator.SetBool("isBubbling", true);
+            bubble3Animator.SetBool("isBubbling", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Fuego")
+        {
+            Debug.Log("Unity salió del fuego");
+
+            bubble1Animator.SetBool("isBubbling", false);
+            bubble2Animator.SetBool("isBubbling", false);
+            bubble3Animator.SetBool("isBubbling", false);
+        }
     }
 }
